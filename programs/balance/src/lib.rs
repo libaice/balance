59f585e606a5600e540781f61baa9a6bad5c0b49
libaice <libaice147@gmail.com>
2024-11-28 21:49:39 +0800

    add sol splitter

--- programs/balance/src/lib.rs
+++ programs/balance/src/lib.rs
@@ -1,6 +1,5 @@
-use core::str;
-
 use anchor_lang::prelude::*;
+use anchor_lang::system_program;
 
 declare_id!("vixF6x1vJt5X3BhFCtUiWxrTpJhmRsvFaJWyqKU1Hmh");
 
@@ -8,16 +7,71 @@ declare_id!("vixF6x1vJt5X3BhFCtUiWxrTpJhmRsvFaJWyqKU1Hmh");
 pub mod balance {
     use super::*;
 
-    pub fn read_balance(ctx: Context<ReadBalance>) -> Result<()> {
-        let balance = ctx.accounts.acct.to_account_info().lamports();
-        //Program log: Balance: 499999994257141640
-        msg!("Balance: {}", balance);
+    pub fn send_sol(ctx: Context<SendSol>, amount: u64) -> Result<()> {
+        let cpi_context = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.signer.to_account_info(),
+                to: ctx.accounts.recipient.to_account_info(),
+            },
+        );
+
+        let res = system_program::transfer(cpi_context, amount);
+        if res.is_ok() {
+            Ok(())
+        } else {
+            Err(Errors::TransferFailed.into())
+        }
+    }
+
+    pub fn split_sol<'a, 'b, 'c, 'info>(
+        ctx: Context<'a, 'b, 'c, 'info, SplitSol<'info>>,
+        amount: u64,
+    ) -> Result<()> {
+        let amount_each_gets = amount / ctx.remaining_accounts.len() as u64;
+        let system_program = &ctx.accounts.system_program;
+
+        for recipient in ctx.remaining_accounts {
+            let cpi_context = system_program::Transfer {
+                from: ctx.accounts.signer.to_account_info(),
+                to: recipient.to_account_info(),
+            };
+
+            let cpi_program = system_program.to_account_info();
+            let cpi_context = CpiContext::new(cpi_program, cpi_context);
+
+            let res = system_program::transfer(cpi_context, amount_each_gets);
+
+            if !res.is_ok() {
+                return Err(Errors::TransferFailed.into());
+            }
+        }
+
         Ok(())
     }
 }
 
+#[error_code]
+pub enum Errors {
+    #[msg("tranfer failed")]
+    TransferFailed,
+}
+
 #[derive(Accounts)]
-pub struct ReadBalance<'info> {
-    ///CHECK: although we read this account's balance, we don't do anything with the information
-    pub acct: UncheckedAccount<'info>,
+pub struct SendSol<'info> {
+    /// CHECK: we do not read or write the data of this account
+    #[account(mut)]
+    recipient: UncheckedAccount<'info>,
+    system_program: Program<'info, System>,
+
+    #[account(mut)]
+    signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct SplitSol<'info> {
+    #[account(mut)]
+    signer: Signer<'info>,
+
+    system_program: Program<'info, System>,
 }
--- tests/balance.ts
+++ tests/balance.ts
@@ -8,11 +8,50 @@ describe("balance", () => {
   const program = anchor.workspace.Balance as Program<Balance>;
   let pubKey = new anchor.web3.PublicKey("6uGWyDVbi5vq76AyNEazXvBnwnvTxu7mLHEW2q5MtwJe");
 
-  it("Is initialized!", async () => {
+  async function printAccountBalance(account) {
+    const balance = await anchor.getProvider().connection.getBalance(account);
+    console.log(`${account} has ${balance / anchor.web3.LAMPORTS_PER_SOL} SOL`);
+  }
 
-    // Program log: Balance: 499999994257141640
-    const tx = await program.methods.readBalance().accounts({acct: pubKey}).rpc();
-    console.log("send qeuery balance tx", tx);
-   
+  it("Send sol to recipient", async () => {
+
+    const recipient = anchor.web3.Keypair.generate();
+
+    await printAccountBalance(recipient.publicKey);
+
+    let amount = new anchor.BN(1 * anchor.web3.LAMPORTS_PER_SOL);
+
+    await program.methods.sendSol(
+      amount
+    ).accounts({ recipient: recipient.publicKey }).rpc();
+
+    await printAccountBalance(recipient.publicKey);
+
   });
+
+
+  it("Send sol to multiple recipients", async () => {
+    const recipient1 = anchor.web3.Keypair.generate();
+    const recipient2 = anchor.web3.Keypair.generate();
+    const recipient3 = anchor.web3.Keypair.generate();
+
+
+    await printAccountBalance(recipient1.publicKey);
+    await printAccountBalance(recipient2.publicKey);
+    await printAccountBalance(recipient3.publicKey);
+
+    const amountMeta1 = {pubkey: recipient1.publicKey, isWritable: true, isSigner: false};
+    const amountMeta2 = {pubkey: recipient2.publicKey, isWritable: true, isSigner: false};
+    const amountMeta3 = {pubkey: recipient3.publicKey, isWritable: true, isSigner: false};
+
+    let amount = new anchor.BN(1 * anchor.web3.LAMPORTS_PER_SOL);
+
+    await program.methods.splitSol(amount).remainingAccounts([amountMeta1, amountMeta2, amountMeta3]).rpc();
+
+    await printAccountBalance(recipient1.publicKey);
+    await printAccountBalance(recipient2.publicKey);
+    await printAccountBalance(recipient3.publicKey);
+  })
+
+
 });
